5e8dd1045c214f1430a25b5c6327c05ea3a11048
kagabojaphet <iradukundajaphet59@gmail.com>
2023-11-29 15:37:44 +0200

    add

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About=()=>{
+ return(
+    <>
+    <div className="about-container">
+       <h1>hhhhhhhhhhhhhh</h1> 
+    </div>
+    </>
+ )
+}
+export default About
--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "../sass/main.scss";
 import Header from "../components/Header";
 import Home from "../components/Home";
+import About from "../components/About";
 
 const Welcome=()=>{
     return(
         <>
         <Header/>
         <Home/>
+        <About/>
         </>
     )
 }
