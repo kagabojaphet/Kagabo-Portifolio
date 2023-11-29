74960834b6725da9ecfa2f3e47a9f385e3deb77b
kagabojaphet <iradukundajaphet59@gmail.com>
2023-11-29 15:59:00 +0200

    add

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,32 @@ const About=()=>{
  return(
     <>
     <div className="about-container">
-       <h1>hhhhhhhhhhhhhh</h1> 
+    <h1>ABOUT</h1>
+        <div class="about-left">
+            <div class="about-image">
+                
+            </div>
+        </div>
+        <div class="about-right">
+            <h2>K.JAPHET</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                 Molestiae odit nobis commodi quaerat ut iure 
+                tempore nemo accusantium, dolorem ad id eius placeat optio 
+                eligendi quibusdam, numquam fugit totam eos
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                 Molestiae odit nobis commodi quaerat ut iure 
+                tempore nemo accusantium, dolorem ad id eius placeat optio 
+                eligendi quibusdam, numquam fugit?</p>
+                <div class="home-icon">
+                    <a href=""><i class='bx bxl-facebook'></i></a>
+                    <a href=""><i class='bx bxl-twitter' ></i></a>
+                    <a href=""><i class='bx bxl-instagram' ></i></a>
+                    <a href=""><i class='bx bxl-linkedin' ></i></a>
+                </div>
+                <div class="btn-container">
+                    <a href="#" class="btn">Read more</a>
+                </div>
+        </div>
     </div>
     </>
  )
