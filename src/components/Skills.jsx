09e9f0f774f132c524d53c79df58432ab3adb85d
kagabojaphet <iradukundajaphet59@gmail.com>
2023-12-02 11:58:22 +0200

    add

---
+++ src/components/Skills.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import {Progress,Space} from "antd";
+
+const Skills=()=>{
+    return(
+        <>
+        <div className="skills-container">
+            <h1>SKILLS</h1>
+            <div className="skills-left">
+                <div className="skills-image">
+                    <img src="" alt="" />
+                </div>
+            </div>
+            <div className="skills-right">
+                <h2>MY SKILLS</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti sit hic quod  
+                    quisquam aliquam tempore similique nemo tenetur necessitatibus. 
+                    Quasi dolore quaerat error incidunt temporibus fugiat
+                     maxime perferendis itaque qui!</p>
+                     <Space wrap className="progress">
+                        <div className="progress-card">
+                        <Progress type="circle" percent={95} />
+                        <h3>HTML</h3>
+                        </div>
+                        <div className="progress-card">
+                        <Progress type="circle" percent={95} />
+                        <h3>CSS</h3>
+                        </div>
+                        <div className="progress-card">
+                        <Progress type="circle" percent={95} />
+                        <h3>JAVASCRIPT</h3>
+                        </div>
+                        <div className="progress-card">
+                        <Progress type="circle" percent={95} />
+                        <h3>PHP</h3>
+                        </div>
+                        <div className="progress-card">
+                        <Progress type="circle" percent={95} />
+                        <h3>MYSQL</h3>
+                        </div>
+                        <div className="progress-card">
+                        <Progress type="circle" percent={95} />
+                        <h3>MONGO DB</h3>
+                        </div>
+                        <div className="progress-card">
+                        <Progress type="circle" percent={95} />
+                        <h3>REACT</h3>
+                        </div>
+                        <div className="progress-card">
+                        <Progress type="circle" percent={95} />
+                        <h3>FIGMA</h3>
+                        </div>
+                    
+                     </Space>
+            </div>
+        </div>
+        </>
+    )
+}
+export default Skills
--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -3,13 +3,15 @@ import "../sass/main.scss";
 import Header from "../components/Header";
 import Home from "../components/Home";
 import About from "../components/About";
-
+import Skills from "../components/Skills";
 const Welcome=()=>{
     return(
         <>
         <Header/>
         <Home/>
         <About/>
+        <Skills/>
+
         </>
     )
 }
