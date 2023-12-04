d2c3eb6ab8f65e99a6fece2b57cf9471812de9ea
kagabojaphet <iradukundajaphet59@gmail.com>
2023-12-04 12:28:03 +0200

    add

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -4,8 +4,8 @@ const Blogs=()=>{
     return(
         <>
         <div className="blogs-container">
-            <h1>BLOGS</h1>
             <div className="blogs-left">
+            <h1>BLOGS</h1>
                 <div className="blogs-image">
                     <img src="" alt="" />
                 </div>
@@ -34,25 +34,6 @@ const Blogs=()=>{
                             <h5>Lorem ipsum dolor sit amet consectetur </h5>
                             <a href="">more</a> 
                         </div>
-                        <div className="blog-news">
-                            <img src='https://images.pexels.com/photos/21627/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=600' alt="" />
-                            <h3>Nyabike</h3>
-                            <h5>Lorem ipsum dolor sit amet consectetur </h5>
-                            <a href="">more</a> 
-                        </div>
-                        <div className="blog-news">
-                            <img src='https://images.pexels.com/photos/21627/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=600' alt="" />
-                            <h3>Nyabike</h3>
-                            <h5>Lorem ipsum dolor sit amet consectetur </h5>
-                            <a href="">more</a> 
-                        </div>
-                        <div className="blog-news">
-                            <img src='https://images.pexels.com/photos/21627/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=600' alt="" />
-                            <h3>Nyabike</h3>
-                            <h5>Lorem ipsum dolor sit amet consectetur </h5>
-                            <a href="">more</a> 
-                        </div>
-
                     </div>
  
             </div>
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,11 +7,41 @@ const Contact=()=>{
             <div className="contact-left">
             <h1>CONTACT</h1>
                 <div className="contact-media">
-                    
+                    <h4>Contact us</h4>
+                    <div className="media-container">
+                        <div className="media">
+                        <i class='bx bxs-location-plus'></i>
+                        <p>Gatsibo,Gatsibo,Gatsibo</p>
+                        </div>
+                        <div className="media">
+                        <i class='bx bxs-phone-call' ></i>
+                        <p>+250789757544</p>
+                        </div>
+                        <div className="media">
+                        <i class='bx bxl-whatsapp'></i>
+                        <p>+250785635633</p>
+                        </div>
+                        <div className="media">
+                        <i class='bx bx-envelope' ></i>
+                        <p>IradukundaJaphet@gmail.com</p>
+                        </div>
+                    </div>
+                    <h4>Thanks !!!</h4>
                 </div>
+                
             </div>
             <div className="contact-right">
+                <h2>MY CONTACT</h2>
+                <div className="contact-form">
+                    <p>Send us a message!</p>
+                    <form action="">
+                      <div className="input-box">
+                      </div>
+                    </form>
+                    <form action="">
 
+                    </form>
+                </div>
             </div>
         </div>
         </>
