768abe8cccbec6e7906604b4c37ef07f1fce21d2
kagabojaphet <iradukundajaphet59@gmail.com>
2024-02-22 13:52:29 +0200

    add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React,{useState} from "react";
 
 const Header=()=>{
+
+    const [navbar,setnavbar]=useState(true)
+    const [menubar,setmenubar]=useState(true)
+
+    function shownavbar(){
+      setnavbar(false)
+    }
+    function closenavbar(){
+    
+
+    }
     return (
         <>
-        <header>
+    <header>
             <div className="logo">
-                <h1>K.JAPHET</h1>
+                <h1>K.JAPHET</h1>                                                                                            
             </div>
-            <ul className="nav-links">
+           ({navbar &&<ul className="nav-links">
                 <li><a href="@" className="active">Home</a></li>
                 <li><a href="@">About</a></li>
                 <li><a href="@">Portifolio</a></li>
@@ -15,8 +26,10 @@ const Header=()=>{
                 <li><a href="@">Blogs</a></li>
                 <li><a href="@">Contact</a></li>
                 <div className="user"><i class='bx bxs-user-circle'></i></div>
-                <div className="menu"><i class='bx bx-menu'></i></div>
+                
             </ul>
+            })
+            <div className="menu" onClick={shownavbar}><i class='bx bx-menu'></i></div>
         </header>
         </>
     )
