2e5dd125d222cdc5219dc8a28f7ec43a31c4611e
kagabojaphet <iradukundajaphet59@gmail.com>
2024-03-04 18:07:15 +0200

    add

--- src/components/Portifolio.jsx
+++ src/components/Portifolio.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Image } from 'antd';
-import jeff from "../assests/k.japhet.jpg"
+import jeff from "../assests/kj.jpg"
 import git from "../assests/git.png";
 
 
@@ -11,13 +11,23 @@ const Portifolio =()=>{
             <div className="port-left">
             <h1>PORTIFOLIO</h1>
             <div className="port-image">
+            <div className="port-left-img">
             <Image className="image" src={jeff}alt="" >
             </Image>
+                </div>
+            
             </div>
             </div>
             <div className="port-right">
                 <h2>PORTIFOLIO</h2>
                 <div className="port-right-cards">
+                    <div className="card">
+                        <img src={git} alt="" />
+                        <div className="card-hover">
+                            <p>My GitHub</p>
+                            <a href="@">Open</a>
+                        </div>
+                    </div>
                     <div className="card">
                         <img src={git} alt="" />
                         <div className="card-hover">
